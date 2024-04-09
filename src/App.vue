eccd562cb6499db98a38c8aabff5d7656258db2f
opilane <priitalas12@gmail.com>
2024-04-09 13:01:33 +0300

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <font-awesome-icon :icon="['fas', 'user-secret']" />
+
   </nav>
   <router-view/>
 </template>
