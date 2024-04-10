85b0368c7c37687f99d5b587501d56b54205162d
Andres <andres.simuland@gmail.com>
2024-04-10 11:36:47 +0300

    Test123.vue

---
+++ src/views/ErrorView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container text-center">
+    <div class="row">
+      <div class="col">
+        <h1>Midagi läks valesti :(</h1>
+      </div>
+    </div>
+    <img alt="Broken" src="../assets/BrokenMedicine.jpg">
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'ErrorView'
+}
+</script>
