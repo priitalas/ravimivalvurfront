2b5aec452117df6f011fdd02bf8c27f29ecc539b
Margit Toovere <margit.toovere@gmail.com>
2024-04-11 16:30:18 +0300

    RAV-1 WIP, registreerimise modal

--- src/components/modal/LoginModal.vue
+++ src/components/modal/LoginModal.vue
@@ -1,45 +1,49 @@
 <template>
-  <Modal ref="modalRef">
-    <template #title>
-      Logi sisse
-    </template>
+  <Modal ref="modalRef" @event-close-modal="resetAllInputFields">
+      <template #title>
+        Logi sisse
+      </template>
 
-    <template #body>
-      <div class="container text-start">
-        <div class="row justify-content-center">
-          <div class="col">
-            <div class="mb-3">
-              <label for="username" class="form-label">Ees- ja perekonnanimi</label>
-              <input v-model="username" type="text" class="form-control" id="username">
-            </div>
-            <div class="mb-3">
-              <label for="password" class="form-label">Salasõna</label>
-              <input v-model="password" type="password" class="form-control" id="password">
+      <template #body>
+        <div class="container text-start">
+          <div class="row justify-content-center">
+            <div class="col">
+              <div class="mb-3">
+                <label for="username" class="form-label">Ees- ja perekonnanimi</label>
+                <input v-model="username" type="text" class="form-control" id="username">
+              </div>
+              <div class="mb-3">
+                <label for="password" class="form-label">Salasõna</label>
+                <input v-model="password" type="password" class="form-control" id="password">
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </template>
+      </template>
 
-    <template #buttons>
-      <button type="submit" class="btn btn-primary text-center text-nowrap">Logi sisse</button>
-    </template>
+      <template #buttons>
+        <button @click="executeLogin" type="submit" class="btn btn-primary text-center text-nowrap">Logi sisse</button>
+        <button @click="openRegistrationModal" type="submit" class="btn btn-warning text-center text-nowrap">Loo uus kasutaja</button>
+      </template>
 
   </Modal>
 </template>
 
 <script>
 import Modal from "@/components/modal/Modal.vue";
+import RegistrationModal from "@/components/modal/RegistrationModal.vue";
+import router from "@/router";
 
 export default {
   name: "LoginModal",
-  components: {Modal},
+  components: {RegistrationModal, Modal},
 
   data() {
     return {
       username: '',
       password: '',
       message: '',
+      isRegistered: false,
       loginResponse: {
         userId: 0,
         roleName: '',
@@ -50,10 +54,68 @@ export default {
         errorCode: ''
       }
     }
-
-
   },
-};
+
+  methods: {
+    openRegistrationModal() {
+      this.$emit('event-open-registration-modal')
+      this.$refs.modalRef.closeModal();
+    },
+
+    allFieldsWithCorrectInput() {
+      return this.username.length > 0 && this.password.length > 0;
+    },
+
+    executeLogin() {
+      if (this.allFieldsWithCorrectInput()) {
+        this.sendLoginRequest();
+      } else {
+        this.displayAllFieldsRequiredAlert();
+      }
+    },
+
+    sendLoginRequest() {
+      this.$http.get('/login', {
+        params: {
+          username: this.username,
+          password: this.password
+        }
+      }
+      ).then(response => {
+        this.loginResponse = response.data
+        sessionStorage.setItem('userId', this.loginResponse.userId)
+        sessionStorage.setItem('roleName', this.loginResponse.roleName)
+        sessionStorage.setItem('userStatus', this.loginResponse.userStatus)
+        this.$emit('event-update-nav-menu')
+        this.resetAllInputFields()
+        this.$refs.modalRef.closeModal()
+        router.push({name: 'doctorRoute'})
+
+      }).catch(error => {
+        this.errorResponse = error.response.data
+        this.handleError(error.response.status)
+      })
+    },
+
+    handleError(statusCode) {
+      if (statusCode === 403 && this.errorResponse.errorCode === 111) {
+        this.message = this.errorResponse.message;
+        setTimeout(this.resetMessage, 4000);
+      } else {
+        router.push({name: 'errorRoute'})
+      }
+    },
+
+    resetAllInputFields() {
+      this.username = ''
+      this.password = ''
+    },
+
+    resetMessage() {
+      this.message = ''
+    },
+  }
+}
 </script>
 
 
