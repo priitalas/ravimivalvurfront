151d4fe97717880669d68aa870e504b648834266
Margit Toovere <margit.toovere@gmail.com>
2024-04-12 19:02:51 +0300

    login modal valmis, registreerimise modal WIP

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import LoginView from "@/views/LoginView.vue";
 import ErrorView from "@/views/ErrorView.vue";
 import DoctorView from "@/views/DoctorView.vue";
 import PatientView from "@/views/PatientView.vue";
