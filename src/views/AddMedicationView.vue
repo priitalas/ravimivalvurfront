b8aa8efa96be00e2f1d7719fb9cc9d4012a2e4d1
Margit Toovere <margit.toovere@gmail.com>
2024-04-16 12:23:10 +0300

    uue ravimi lisamine WIP

--- src/views/AddMedicationView.vue
+++ src/views/AddMedicationView.vue
@@ -11,31 +11,29 @@
       <div class="col col-5 justify-content-start">
         <div class="input-group mb-3">
           <span class="input-group-text">Ravimi nimi</span>
-          <input v-model="medicationName" type="text" class="form-control">
+          <input v-model="medicationInfo.medicationName" type="text" class="form-control">
         </div>
         <div class="input-group mb-3">
           <span class="input-group-text">Kirjeldus</span>
-          <textarea v-model="description" type="text" class="form-control"></textarea>
+          <textarea v-model="medicationInfo.description" type="text" class="form-control"></textarea>
         </div>
         <div class="input-group mb-3">
           <span class="input-group-text">Lisainfo</span>
-          <textarea v-model="note" type="text" class="form-control"></textarea>
+          <input v-model="medicationInfo.note" type="text" class="form-control">
+        </div>
+        <div class="col col-5 justify-content-start">
+          <select v-model="medicationInfo.unitId" class="form-select">
+            <option selected value="0">Vali ühik</option>
+            <option v-for="unit in units" :value="unit.unitId" :key="unit.unitId">{{ unit.unitName }}</option>
+          </select>
+        </div>
+        <div class="col col-7 justify-content-start">
+          <label for="imageData" class="form-label" accept="image/x-png,image/jpeg,image/gif">Vali pilt</label>
+          <input class="form-control" type="file" id="imageData">
         </div>
       </div>
     </div>
-    <div class="row justify-content-center mt-3">
-      <div class="dropdown">
-        <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" >
-          Vali ühik
-        </button>
-        <ul class="dropdown-menu">
-          <li><a class="dropdown-item" href="#">Action</a></li>
-          <li><a class="dropdown-item" href="#">Another action</a></li>
-          <li><a class="dropdown-item" href="#">Something else here</a></li>
-        </ul>
-      </div>
 
-    </div>
   </div>
 </template>
 
@@ -50,11 +48,11 @@ export default {
 
   data() {
     return {
-      medicationInfo:{
+      medicationInfo: {
         medicationName: '',
         description: '',
         note: '',
-        unitId: '',
+        unitId: 0,
         imageData: ''
       },
       errorMessage: '',
@@ -63,7 +61,7 @@ export default {
       selectedUnitId: '',
       units: [
         {
-          unitId: '',
+          unitId: 0,
           unitName: ''
         }
       ]
@@ -72,7 +70,7 @@ export default {
 
   methods: {
 
-    sendUnitsRequest(){
+    sendUnitsRequest() {
       this.$http.get('/units')
           .then(response => {
             this.units = response.data
