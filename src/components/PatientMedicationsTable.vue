c56bfaa2b43166ec67cdddcd68651bb320778ad1
Andres <andres.simuland@gmail.com>
2024-04-22 09:24:47 +0300

    Patient current medications view WIP

---
+++ src/components/PatientMedicationsTable.vue
@@ -0,0 +1,67 @@
+<template>
+  <table class="table table-striped table-primary table-hover" style="vertical-align: middle;">
+    <thead>
+    <tr>
+      <th scope="col">Pilt</th>
+      <th scope="col">Nimi</th>
+      <th scope="col">Kogus</th>
+      <th scope="col">Ühik</th>
+      <th scope="col">Lisainfo</th>
+      <th scope="col">Märgi võetuks</th>
+    </tr>
+    </thead>
+
+    <tbody>
+    <tr v-for="patientCurrentMedication in patientCurrentMedications" :key="patientCurrentMedication.medicationId">
+      <td>Siia tuleb pilt</td>
+      <td>Aspirin</td>
+      <td>1</td>
+      <td>Tablett</td>
+      <td>Võtta enne sööki</td>
+      <td>
+        <button type="button" class="btn btn-danger btn-lg">Märgi võetuks</button>
+      </td>
+    </tr>
+    </tbody>
+
+  </table>
+</template>
+<script>
+export default {
+  name: 'PatientMedicationsTable',
+  data() {
+    return {
+
+      patientCurrentMedications: [
+        // Swaggeri jsonist kontrolli üle:
+        medicationPlanId: 0,
+        medicationId: 0,
+    medicationName: '',
+        medicationImageData: '',
+        quantity: 0,
+        medicationUnitName: '',
+        medicationNote: '',
+        timeSlotStatus: ''
+      ]
+
+    }
+  },
+  methods: {
+
+    sendGetPatientCurrentMedicationsRequest: function () {
+      this.$http.get("/patient/medications")
+          .then(response => {
+            this.patientCurrentMedications = response.data
+          })
+          .catch(error => {
+            const errorResponseBody = error.response.data
+          })
+    },
+
+  },
+
+  beforeMount() {
+    this.sendGetPatientCurrentMedicationsRequest()
+  }
+}
+</script>
--- src/views/PatientView.vue
+++ src/views/PatientView.vue
@@ -7,32 +7,7 @@
     <div class="row justify-content-center">
       <div class="col-md-12">
 
-        <table v-if="quantity>0" class="table table-striped table-primary table-hover" style="vertical-align: middle;">
-          <thead>
-          <tr>
-            <th scope="col">Pilt</th>
-            <th scope="col">Nimi</th>
-            <th scope="col">Kogus</th>
-            <th scope="col">Ühik</th>
-            <th scope="col">Lisainfo</th>
-            <th scope="col">Märgi võetuks</th>
-          </tr>
-          </thead>
-
-          <tbody>
-          <tr>
-            <td>Siia tuleb pilt</td>
-            <td>Aspirin</td>
-            <td>1</td>
-            <td>Tablett</td>
-            <td>Võtta enne sööki</td>
-            <td>
-              <button type="button" class="btn btn-danger btn-lg">Märgi võetuks</button>
-            </td>
-          </tr>
-          </tbody>
-
-        </table>
+        <PatientMedicationsTable/>
 
       </div>
     </div>
@@ -41,19 +16,15 @@
 
 </template>
 <script>
+import PatientMedicationsTable from "@/components/PatientMedicationsTable.vue";
+
 export default {
   name: "PatientView",
+  components: {PatientMedicationsTable},
 
   data() {
     return {
-    // medicationPlanId: 0,
-    // medicationId: 0,
-    // medicationName: '',
-    // medicationImageData: '',
-    // quantity: 0,
-    // medicationUnitName: '',
-    // medicationNote: '',
-    // timeSlotStatus: ''
+
     }
   },
 
