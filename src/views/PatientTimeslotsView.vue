fb22b7447c4e3a2cae2b8898c5c557bf39ead7cd
Margit Toovere <margit.toovere@gmail.com>
2024-04-28 17:00:33 +0300

    ravikuuri ajasloti kustutamine

--- src/views/PatientTimeslotsView.vue
+++ src/views/PatientTimeslotsView.vue
@@ -31,45 +31,52 @@
       </div>
     </div>
     <p></p>
-      <div class="row justify-content-center">
-        <div class="col-8">
-          <table class="table rounded-table">
-            <thead>
-            <tr>
-              <th scope="col">Aja algus</th>
-              <th scope="col">Aja lõpp</th>
-              <th scope="col">Doos</th>
-              <th scope="col">Muuda</th>
-              <th scope="col">Kustuta</th>
-            </tr>
-            </thead>
-            <tbody>
-            <tr v-for="timeslot in timeslots" :key="timeslot.timeslotId">
-              <td>{{ timeslot.slotStart }}</td>
-              <td>{{ timeslot.slotEnd }}</td>
-              <td>{{ timeslot.quantity }}</td>
-              <td style="width:10%; text-align: center; justify-content: center;">
-                <font-awesome-icon class="link-custom cursor-pointer me-lg-2"
-                                   :icon="['fas', 'pen-to-square']"/>
-              </td>
-              <td style="width:10%; text-align: center; justify-content: center;">
-                <font-awesome-icon class="link-custom cursor-pointer"
-                                   :icon="['fas', 'trash']"/>
-              </td>
-            </tr>
-            </tbody>
-          </table>
-        </div>
+    <div class="row justify-content-center">
+      <div class="col-3">
+        <AlertDanger :message="errorMessage"/>
       </div>
     </div>
+    <div class="row justify-content-center">
+      <div class="col-8">
+        <table class="table rounded-table">
+          <thead>
+          <tr>
+            <th scope="col">Aja algus</th>
+            <th scope="col">Aja lõpp</th>
+            <th scope="col">Doos</th>
+            <th scope="col">Muuda</th>
+            <th scope="col">Kustuta</th>
+          </tr>
+          </thead>
+          <tbody>
+          <tr v-for="timeslot in timeslots" :key="timeslot.timeslotId">
+            <td>{{ timeslot.slotStart }}</td>
+            <td>{{ timeslot.slotEnd }}</td>
+            <td>{{ timeslot.quantity }}</td>
+            <td style="width:10%; text-align: center; justify-content: center;">
+              <font-awesome-icon class="link-custom cursor-pointer me-lg-2"
+                                 :icon="['fas', 'pen-to-square']"/>
+            </td>
+            <td style="width:10%; text-align: center; justify-content: center;">
+              <font-awesome-icon @click="goToDeleteTimeslot(timeslot.timeslotId)" class="link-custom cursor-pointer"
+                                 :icon="['fas', 'trash']"/>
+            </td>
+          </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 import {useRoute} from "vue-router";
 import router from "@/router";
+import AlertDanger from "@/components/alert/AlertDanger.vue";
 
 export default {
   name: "PatientTimeslotsView",
+  components: {AlertDanger},
 
   data() {
     return {
@@ -78,6 +85,7 @@ export default {
       patientFirstName: useRoute().query.patientFirstName,
       patientLastName: useRoute().query.patientLastName,
       medicationName: useRoute().query.medicationName,
+      errorMessage: '',
 
       addedTimeslot: {
         medicationPlanId: useRoute().query.medicationPlanId,
@@ -123,6 +131,29 @@ export default {
         router.push({name: 'errorRoute'})
       })
     },
+
+    goToDeleteTimeslot(selectedTimeslot) {
+      this.$http.delete("/medication-plans/patient/time-slot", {
+            params: {
+              timeslotId: selectedTimeslot
+            }
+          }
+      ).then(response => {
+        this.errorMessage = "Ravimi võtmisaeg kustutatud"
+        setTimeout(this.resetMessage, 2000);
+        this.updateTimeslotsTable()
+      }).catch(error => {
+        router.push({name: 'errorRoute'})
+      })
+    },
+
+    resetMessage() {
+      this.errorMessage = ""
+    },
+  },
+
+  beforeMount() {
+    this.updateTimeslotsTable()
   }
 }
 </script>
